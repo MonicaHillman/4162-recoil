c1b395a42eaf39eae2197e535d559310e92a5323
monica hillman <mmhillman1@ucs.br>
2025-01-25 22:18:04 -0300

    aula 03

---
+++ src/hooks/useContatos.js
@@ -0,0 +1,32 @@
+import { useRecoilState } from "recoil";
+import { contatosState } from "../atoms/contatosState"
+import { apiContatos } from "../api/api"
+import { useEffect } from "react";
+
+export function useContatos() {
+    const [contatos, setContatos] = useRecoilState(contatosState)
+
+    const fetchContatos = async () => {
+        return apiContatos.resgatarContatos().then((dadosDosContatos) => {
+            setContatos(dadosDosContatos);
+        })
+    }
+
+    const addContatos = async (contato) => {
+        return apiContatos.criar(contato).then((novoContato) => {
+            setContatos((listaAntiga) => [...listaAntiga, novoContato])
+
+            return novoContato;
+        })
+    }
+
+    useEffect(() => {
+        fetchContatos();
+    }, [])
+
+    return {
+        contatos,
+        fetchContatos,
+        addContatos
+    }
+}
--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -7,6 +7,7 @@ import Container from "../../components/Container/Container";
 import BotaoVoltar from "../../components/BotaoVoltar/BotaoVoltar";
 import Formulario from "../../components/Formulario/Formulario";
 import Titulo from "../../components/Titulo/Titulo";
+import { useContatos } from "../../hooks/useContatos";
 
 
 function Cadastro() {
@@ -24,9 +25,14 @@ function Cadastro() {
       [id]: value,
     }));
   };
-
-  const handleSubmit = (e) => {
-    console.log("Formulário enviado!");
+  const { addContatos } = useContatos();
+  const handleSubmit = (evento) => {
+    evento.preventDefault();
+    addContatos(dadosDoFormulario).then(() => {
+      navigate("/")
+    }).catch(() => {
+      console.error("Erro ao adicionar o contato");
+    })
   };
 
 
